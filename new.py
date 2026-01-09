4e61ca82e661c313c163747bf8bee3029ab649e8
Prajwol Gynawali <111718164+Prajwol-Gynawali@users.noreply.github.com>
2026-01-09 08:03:24 +0000

    Use Streamlit secrets for Google Sheets credentials

--- new.py
+++ new.py
@@ -38,7 +38,10 @@ def connect_sheets():
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    st.secrets["gcp_service_account"], scope
+)
+
     client = gspread.authorize(creds)
 
     return {
